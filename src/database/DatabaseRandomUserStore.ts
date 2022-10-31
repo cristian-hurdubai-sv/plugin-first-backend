707c1ac5973a889a3900fe1ae8175e8d1f816e82
Cristian Hurdubai <cristian.hurdubai@softvision.com>
2022-10-31 19:17:50 +0200

    initial commit

---
+++ src/database/DatabaseRandomUserStore.ts
@@ -0,0 +1,69 @@
+import { apiOptions, RandomUserRow, RawUser } from './types';
+
+import { v4 as uuid } from 'uuid';
+
+import {
+  PluginDatabaseManager,
+} from '@backstage/backend-common';
+
+import { Knex } from 'knex';
+import { applyDatabaseMigrations } from './migrations';
+
+export class DatabaseRandowUserStore {
+  private constructor(private readonly db: Knex) {}
+
+  public static async create(
+    database: PluginDatabaseManager,
+  ): Promise<DatabaseRandowUserStore> {
+    const knex = await database.getClient();
+
+    if (!database.migrations?.skip) {
+      await applyDatabaseMigrations(knex);
+    }
+    return new DatabaseRandowUserStore(knex);
+  }
+
+  async transaction<T>(fn: (tx: Knex.Transaction) => Promise<T>): Promise<T> {
+    return await this.db.transaction(fn);
+  }
+
+  async getTransaction(): Promise<Knex.Transaction> {
+    return this.db.transaction();
+  }
+
+  public async insert(tx: Knex.Transaction, randomUsers: RawUser[]) {
+    await tx('random_user').insert(
+        randomUsers.map(randomUser => ({
+            first_name: randomUser.name.first,
+            last_name: randomUser.name.last,
+            email: randomUser.email,
+            phone: randomUser.phone,
+            avatar: randomUser.picture.medium,
+            nat: randomUser.nat,
+            gender: randomUser.gender,
+            id: uuid(),
+            target: randomUser.location?.state ?? ""
+        })),
+      );
+  }
+
+  public async getAll(): Promise<RandomUserRow[]> {
+    return await this.db('random_user')
+        .select('*');
+  }
+
+  public async getAllByFilter(options: apiOptions): Promise<RandomUserRow[]> {
+    const query = this.db('random_user')
+        .select('*');
+    
+    return await query;
+        // .paginate(options.limit, options.offset);
+  }
+
+  public async get(id: string): Promise<RandomUserRow[]> {
+    return await this.db('random_user')
+        .select('*')
+        .where('id', id)
+        .first();
+  }
+}
---
+++ src/service/router.ts
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { errorHandler, PluginDatabaseManager } from '@backstage/backend-common';
+import express from 'express';
+import fetch from 'cross-fetch';
+import Router from 'express-promise-router';
+import { Logger } from 'winston';
+import * as userList from "../data/users.json";
+import { DatabaseRandowUserStore } from '../database';
+import { RawUser } from '../database/types';
+
+export interface RouterOptions {
+  logger: Logger;
+  database: PluginDatabaseManager;
+}
+
+export async function createRouter(
+  options: RouterOptions,
+): Promise<express.Router> {
+  const { logger } = options;
+  const router = Router();
+  router.use(express.json());
+
+  const dbHandler: DatabaseRandowUserStore = await DatabaseRandowUserStore.create(options.database);
+
+  router.get('/get-random-users', async (_, response) => {
+    const results = await fetch('https://randomuser.me/api/?results=50');
+    const data = await results.json();
+    // response.send(data.results);
+    await dbHandler.transaction( async tx => {
+      await dbHandler.insert(tx, data.results as RawUser[]);
+    })
+    response.send(data.results);
+  });
+
+  router.get('/get-all', async (_, response) => {
+    const data = await dbHandler.getAll();
+    response.send(data);
+  });
+
+  router.get('/users', async (_, response) => {
+    const data = await dbHandler.getAll();
+    response.send(data);
+  });
+
+  router.get('/users/:id', async (req, response) => {
+    const userId: string = req.params.id || "";
+    const data = await dbHandler.get(userId);
+    response.send(data);
+  });
+
+
+  router.get('/health', (_, response) => {
+    logger.info('PONG!');
+    response.send({ status: 'ok' });
+  });
+  router.use(errorHandler());
+  return router;
+}
